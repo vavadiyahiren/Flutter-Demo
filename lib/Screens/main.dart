94f7de421d840d2ae7570d39f9de89ae8f527541
ubuntu <vavadiyahiren@gmail.com>
2019-11-22 13:57:27 +0530

    Login Screen modified

--- lib/Screens/main.dart
+++ lib/Screens/main.dart
@@ -81,151 +81,178 @@ class AfterSplash extends StatelessWidget {
     return new Scaffold(
       body: Stack(
         children: <Widget>[
-          Center(
-            child: Form(
-              key: _formKey,
-              child: new Column(
-                children: <Widget>[
-                  Container(
-                    height: height * 0.30,
-                    child: Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 18.0),
-                        child: Text(
-                          "Login",
-                          style: TextStyles.LoginTextStyle,
+          SingleChildScrollView(
+            child: Center(
+              child: Form(
+                key: _formKey,
+                child: new Column(
+                  children: <Widget>[
+                    Container(
+                      height: height * 0.30,
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 18.0),
+                          child: Text(
+                            "Login",
+                            style: TextStyles.LoginTextStyle,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 18),
-                      child: Text(
-                        "Please sign in to continue.",
-                        style: TextStyles.LoginText,
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 18),
+                        child: Text(
+                          "Please sign in to continue.",
+                          style: TextStyles.LoginText,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 50.0, left: 18, right: 18),
-                    child: Material(
-                      elevation: 10,
-                      child: Center(
-                        child: new TextFormField(
-                          scrollPadding: EdgeInsets.only(left: 50),
-                          cursorColor: Colors.black,
-                          controller: _userName,
-                          decoration: new InputDecoration(
-                            prefixIcon: const Icon(
-                              Icons.email,
+                    Padding(
+                      padding: EdgeInsets.only(top: 50.0, left: 18, right: 18),
+                      child: Material(
+                        elevation: 10,
+                        child: Center(
+                          child: new TextFormField(
+                            scrollPadding: EdgeInsets.only(left: 50),
+                            cursorColor: Colors.black,
+                            controller: _userName,
+                            decoration: new InputDecoration(
+                              prefixIcon: const Icon(
+                                Icons.email,
+                              ),
+                              labelText: "Enter Email",
+                              focusedBorder: InputBorder.none,
+                              border: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                              //fillColor: Colors.green
                             ),
-                            labelText: "Enter Email",
+                            validator: (val) {
+                              if (val.length == 0) {
+                                return "Email cannot be empty";
+                              } else if (!val.contains("@")) {
+                                return "Please Enter valid email";
+                              } else {
+                                return null;
+                              }
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            style: new TextStyle(
+                              fontFamily: "Ubuntu-M",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0, left: 18, right: 18),
+                      child: Material(
+                        elevation: 10,
+                        child: new TextFormField(
+                          obscureText: true,
+                          controller: _password,
+                          decoration: new InputDecoration(
+                            suffix: Padding(
+                              padding: EdgeInsets.only(right: 15),
+                              child: Text("Forgot"),
+                            ),
+                            suffixStyle: TextStyle(
+                                fontSize: 15,
+                                color: Colors.blueAccent,
+                                fontFamily: 'Ubuntu-M',
+                                fontWeight: FontWeight.w500),
                             focusedBorder: InputBorder.none,
                             border: InputBorder.none,
                             errorBorder: InputBorder.none,
-                            //fillColor: Colors.green
+                            labelText: "Enter Password",
+                            prefixIcon: const Icon(
+                              Icons.lock,
+                            ),
                           ),
                           validator: (val) {
-                            if (val.length == 0) {
-                              return "Email cannot be empty";
-                            } else if (!val.contains("@")) {
-                              return "Please Enter valid email";
+                            if (val.length < 3) {
+                              return "password should be more then 4 character";
                             } else {
                               return null;
                             }
                           },
-                          keyboardType: TextInputType.emailAddress,
+                          keyboardType: TextInputType.visiblePassword,
                           style: new TextStyle(
                             fontFamily: "Ubuntu-M",
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.0, left: 18, right: 18),
-                    child: Material(
-                      elevation: 10,
-                      child: new TextFormField(
-                        obscureText: true,
-                        controller: _password,
-                        decoration: new InputDecoration(
-                          suffix: Padding(
-                            padding: EdgeInsets.only(right: 15),
-                            child: Text("Forgot"),
-                          ),
-                          suffixStyle: TextStyle(
-                              fontSize: 15,
-                              color: Colors.blueAccent,
-                              fontFamily: 'Ubuntu-M',
-                              fontWeight: FontWeight.w500),
-                          focusedBorder: InputBorder.none,
-                          border: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          labelText: "Enter Password",
-                          prefixIcon: const Icon(
-                            Icons.lock,
-                          ),
-                        ),
-                        validator: (val) {
-                          if (val.length < 3) {
-                            return "password should be more then 4 character";
-                          } else {
-                            return null;
-                          }
-                        },
-                        keyboardType: TextInputType.visiblePassword,
-                        style: new TextStyle(
-                          fontFamily: "Ubuntu-M",
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 18.0, left: 18),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        !_data.isAuthenticated
-                            ? "Invalid Username or Password"
-                            : "",
-                        textAlign: TextAlign.center,
-                        style: TextStyles.LoginErrorStyle,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Container(
-                      alignment: Alignment.centerRight,
-                      child: RaisedButton(
-                        color: Colors.blue,
-                        splashColor: Colors.blueAccent,
-                        elevation: 5,
-                        padding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(25.0),
-                        ),
-                        textColor: Colors.white,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 18.0, left: 18),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
                         child: Text(
-                          "Sign In",
-                          style:
-                              TextStyle(fontSize: 20, fontFamily: "Ubuntu-M"),
+                          !_data.isAuthenticated
+                              ? "Invalid Username or Password"
+                              : "",
+                          textAlign: TextAlign.center,
+                          style: TextStyles.LoginErrorStyle,
                         ),
-                        onPressed: () {
-                          if (_formKey.currentState.validate()) {
-                            _loginPress(context, _data);
-                          }
-                        },
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Container(
+                        alignment: Alignment.centerRight,
+                        child: RaisedButton(
+                          color: Colors.blue,
+                          splashColor: Colors.blueAccent,
+                          elevation: 5,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 40),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                          ),
+                          textColor: Colors.white,
+                          child: Text(
+                            "Sign In",
+                            style:
+                                TextStyle(fontSize: 20, fontFamily: "Ubuntu-M"),
+                          ),
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              _loginPress(context, _data);
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Don't have an account ? ",
+                              style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 18,
+                                  fontFamily: "Ubuntu-C"),
+                            ),
+                            Text("Sign Up",
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w900,
+                                    fontFamily: "Ubuntu-C"))
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           )
